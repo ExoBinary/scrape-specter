d5cad0ff9057184ba0601a6d4ed983667fd1790c
abdullahhcms <91176669+DawoodKMasood@users.noreply.github.com>
2024-03-09 17:46:36 +0500

    Update database.py

--- modules/database.py
+++ modules/database.py
@@ -1,12 +1,16 @@
 from sqlalchemy import create_engine, MetaData, Table, Column, Integer, String, DateTime, ForeignKey, Text
 from sqlalchemy.sql import func
 from sqlalchemy.orm import declarative_base, sessionmaker
+from dotenv import load_dotenv
+import os
+
+# Load environment variables from .env file
+load_dotenv()
 
 Base = declarative_base()
 
 class CrawledDomains(Base):
     __tablename__ = 'crawled_domains'
-
     id = Column(Integer, primary_key=True)
     domain_url = Column(String, nullable=False)
     status = Column(String, nullable=False)
@@ -15,7 +19,6 @@ class CrawledDomains(Base):
 
 class CrawledPages(Base):
     __tablename__ = 'crawled_pages'
-
     id = Column(Integer, primary_key=True)
     page_url = Column(Text, nullable=False)
     content = Column(Text)
@@ -23,8 +26,8 @@ class CrawledPages(Base):
     createdat = Column(DateTime, default=func.current_timestamp())
     updatedat = Column(DateTime, default=func.current_timestamp(), onupdate=func.current_timestamp())
 
-# Update the URL below with your actual database connection string
-DATABASE_URL = "postgresql://postgres:awaswe123@localhost/chatbot"
+# Get DATABASE_URL from environment variable
+DATABASE_URL = os.getenv("DATABASE_URL")
 
 engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
